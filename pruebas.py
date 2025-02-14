869ecdcd0870571079850e1f04c829cc32d209be
Leo Diaz <leodiazdt@gmail.com>
2025-02-14 12:28:38 -0300

    Sustitui y simplifique el dockerfile para railway

---
+++ pruebas.py
@@ -0,0 +1,7 @@
+import fastapi
+import uvicorn
+import mlflow
+
+print(f"FastAPI version: {fastapi.__version__}")
+print(f"Uvicorn version: {uvicorn.__version__}")
+print(f"MLflow version: {mlflow.__version__}")
