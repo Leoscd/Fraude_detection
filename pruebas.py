df31790233a963ddf6ee473c068a6157e67d3883
Leo Diaz <leodiazdt@gmail.com>
2025-02-21 12:41:05 -0300

    cambios a la estructura de app.py y health

--- pruebas.py
+++
@@ -1,7 +0,0 @@
-import fastapi
-import uvicorn
-import mlflow
-
-print(f"FastAPI version: {fastapi.__version__}")
-print(f"Uvicorn version: {uvicorn.__version__}")
-print(f"MLflow version: {mlflow.__version__}")
